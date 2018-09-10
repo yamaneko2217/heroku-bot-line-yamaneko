535b289754590dda36756ceaba198f8cbffc31dd
yamaneko <ntyt1417@outlook.jp>
2018-09-10 21:56:35 +0900

    h

--- app.js
+++ app.js
@@ -25,18 +25,18 @@ function createReplyMessage(input) {
   if (input === '判定'){
     if(user === 'グー'){
       if(bot === 'グー'){text = "あいこ";}
-      else if(bot === 'チョキ'){text = "勝利";}
-      else if(bot === 'パー'){text ="負け";}
+      if(bot === 'チョキ'){text = "勝利";}
+      if(bot === 'パー'){text ="負け";}
     }
      else if(user === 'チョキ'){
       if(bot === 'グー'){text = "負";}
-      else if(bot === 'チョキ'){text ="あいこ";}
-      else if(bot === 'パー'){text = "勝利";}
+      if(bot === 'チョキ'){text ="あいこ";}
+      if(bot === 'パー'){text = "勝利";}
     } 
      else if(user === 'パー'){
       if(bot === 'グー'){text = "勝利";}
-      else if(bot === 'チョキ'){text = "負け";}
-      else if(bot === 'パー');{text = "あいこ";}
+      if(bot === 'チョキ'){text = "負け";}
+      if(bot === 'パー'){text = "あいこ";}
     }
   } else if (hands.indexOf(input) === -1) {
     // ユーザーが入力した内容が「グー、チョキ、パー」以外だった場合
