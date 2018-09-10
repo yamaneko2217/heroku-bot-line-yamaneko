5b825048afde3a3d5802c5d7b2b620b46211236c
yamaneko <ntyt1417@outlook.jp>
2018-09-10 22:17:16 +0900

    ｆ

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ function createReplyMessage(input) {
   const hands = ["グー", "チョキ", "パー"];
   // 返信メッセージを入れる変数
   let text;
+  let result;
 
   // 配列.indexOf(引数) =>
   //   引数が配列の何番目（0始まり）にあるかを返す
@@ -39,7 +40,7 @@ function createReplyMessage(input) {
       if(bot === 'チョキ'){text = "負け";}
       if(bot === 'パー'){text = "あいこ";}
     }
-    
+
   } else if (hands.indexOf(input) === -1) {
     // ユーザーが入力した内容が「グー、チョキ、パー」以外だった場合
     text = "グー・チョキ・パーのどれかを入力してね";
@@ -47,6 +48,22 @@ function createReplyMessage(input) {
   } else {
     // 手からランダムに一つ選択
     text = hands[Math.floor(hands.length * Math.random())];
+    if(input === 'グー'){
+      if(text === 'グー'){ result = "あいこ";}
+      if(text === 'チョキ'){ result = "勝ち";}
+      if(text === 'パー'){ result = "負け";}
+    
+    }else if(input === 'チョキ'){
+      if(text === 'グー'){ result = "負け";}
+      if(text === 'チョキ'){ result = "あいこ";}
+      if(text === 'パー'){ result = "勝ち";}
+    
+    }else if(input ==='パー'){
+      if(text === 'グー'){ result = "勝ち";}
+      if(text === 'チョキ'){ result = "負け";}
+      if(text === 'パー'){ result = "あいこ";}
+
+    }
   }
 
   prev.user = input;
